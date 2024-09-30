1e49a3b7d37c002b539e90f8bcd099b214c7041b
Maksim Berdnikau <max.berd.1900@gmail.com>
2024-09-30 11:16:28 +0300

    Добавление класса RepositoryManager

---
+++ Contracts/IRepositoryManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts.Repositories;
+
+namespace Contracts;
+
+public interface IRepositoryManager
+{
+	ICourseRepository Courses { get; }
+	IEmployeeRepository Employees { get; }
+	IJobTitleRepository JobTitles { get; }
+	IPaymentRepository Payments { get; }
+	IStudentRepository Students { get; }
+	Task SaveAsync();
+}
---
+++ LanguageCourses.Persistence/RepositoryManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+using Contracts.Repositories;
+using LanguageCourses.Persistence.Repositories;
+
+namespace LanguageCourses.Persistence;
+
+public class RepositoryManager : IRepositoryManager
+{
+	private readonly LanguageCoursesContext _dbContext;
+	private readonly Lazy<IEmployeeRepository> _empRep;
+	private readonly Lazy<IJobTitleRepository> _jobRep;
+	private readonly Lazy<ICourseRepository> _coursesRep;
+	private readonly Lazy<IStudentRepository> _studRep;
+	private readonly Lazy<IPaymentRepository> _paymRep;
+
+	public RepositoryManager(LanguageCoursesContext dbContext)
+	{
+		_dbContext = dbContext;
+		_empRep = new Lazy<IEmployeeRepository>(() => new EmployeeRepository(_dbContext));
+		_jobRep = new Lazy<IJobTitleRepository>(() => new JobTitleRepository(_dbContext));
+		_coursesRep = new Lazy<ICourseRepository>(() => new CourseRepository(_dbContext));
+		_studRep = new Lazy<IStudentRepository>(() => new StudentRepository(_dbContext));
+		_paymRep = new Lazy<IPaymentRepository>(() => new PaymentRepository(_dbContext));
+	}
+
+	public IEmployeeRepository Employees => _empRep.Value;
+	public IJobTitleRepository JobTitles => _jobRep.Value;
+	public ICourseRepository Courses => _coursesRep.Value;
+	public IStudentRepository Students => _studRep.Value;
+	public IPaymentRepository Payments => _paymRep.Value;
+
+	public async Task SaveAsync() => await _dbContext.SaveChangesAsync();
+}
